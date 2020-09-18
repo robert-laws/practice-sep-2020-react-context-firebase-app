3117abefeefb1e560738beb4812ccd76d289ce36
Robert Laws <roblaws@me.com>
2020-09-18 16:42:27 +0300

    add router to project and move auth folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <Router>
+      <div className='App'>App</div>
+    </Router>
+  );
 }
 
 export default App;
