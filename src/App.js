34a7fc900fe6c03c967a4325cb0a77ed1040ed73
Robert Laws <roblaws@me.com>
2020-09-19 09:35:12 +0300

    add ProjectDetail component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar } from './components/layout';
 import { Dashboard } from './components/dashboard';
+import { ProjectDetails } from './components/projects';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <div className='App'>
         <Navbar />
         <Switch>
-          <Route path='/' component={Dashboard} />
+          <Route exact path='/' component={Dashboard} />
+          <Route path='/project/:id' component={ProjectDetails} />
         </Switch>
       </div>
     </Router>
--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export const ProjectDetails = () => {
-  return <div>Project Details</div>;
+  const { id } = useParams();
+
+  return (
+    <div className='container section project-details'>
+      <div className='card z-depth-0'>
+        <div className='card-content'>
+          <span className='card-title'>Project Title - {id}</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+            voluptate, tenetur similique libero deleniti laudantium omnis
+            quaerat iste voluptates ipsum!
+          </p>
+        </div>
+        <div className='card-action grey lighten-4 grey-text'>
+          <div>Posted by Bob Cobb</div>
+          <div>19th September, 9am</div>
+        </div>
+      </div>
+    </div>
+  );
 };
