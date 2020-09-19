cb7ca7747e15eb4ff3bbf5a236eca705b5c537c9
Robert Laws <roblaws@me.com>
2020-09-19 09:26:49 +0300

    add Project component

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export const Project = () => {
-  return <div>Project</div>;
+  return (
+    <div className='card z-depth-0 project-summary'>
+      <div className='card-content grey-text text-darken-3'>
+        <span className='card-title'>Project Title</span>
+        <p>Posted by Bob Cobb</p>
+        <p className='grey-text'>19 September, 9am</p>
+      </div>
+    </div>
+  );
 };
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,29 +1,12 @@
 import React from 'react';
+import { Project } from './';
 
 export const ProjectList = () => {
   return (
     <div className='project-list section'>
-      <div className='card z-depth-0 project-summary'>
-        <div className='card-content grey-text text-darken-3'>
-          <span className='card-title'>Project Title</span>
-          <p>Posted by Bob Cobb</p>
-          <p className='grey-text'>19 September, 9am</p>
-        </div>
-      </div>
-      <div className='card z-depth-0 project-summary'>
-        <div className='card-content grey-text text-darken-3'>
-          <span className='card-title'>Project Title</span>
-          <p>Posted by Bob Cobb</p>
-          <p className='grey-text'>19 September, 9am</p>
-        </div>
-      </div>
-      <div className='card z-depth-0 project-summary'>
-        <div className='card-content grey-text text-darken-3'>
-          <span className='card-title'>Project Title</span>
-          <p>Posted by Bob Cobb</p>
-          <p className='grey-text'>19 September, 9am</p>
-        </div>
-      </div>
+      <Project />
+      <Project />
+      <Project />
     </div>
   );
 };
